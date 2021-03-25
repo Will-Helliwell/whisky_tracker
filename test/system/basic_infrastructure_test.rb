603cc8f5e5fc2d027f2288ab42c731bfee396be5
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-03-25 16:54:57 +0000

    Add feature test for basic infrastructure

---
+++ test/system/basic_infrastructure_test.rb
@@ -0,0 +1,8 @@
+require 'application_system_test_case'
+
+class BasicInfrastructureTest < ApplicationSystemTestCase
+  test "User can visit homepage" do
+      visit root_path
+      assert_text "Welcome to Whisky Tracker"
+  end
+end
