b2acc1557001b2fbd6aba9216b82d2bcc3220255
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-03-25 17:13:58 +0000

    Autocorrect offenses with rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'home#index'
-  get '/home', to: "home#index"
+  get '/home', to: 'home#index'
 end
--- test/system/basic_infrastructure_test.rb
+++ test/system/basic_infrastructure_test.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'application_system_test_case'
 
 class BasicInfrastructureTest < ApplicationSystemTestCase
-  test "User can visit homepage" do
-      visit root_path
-      assert_text "Welcome to Whisky Tracker"
+  test 'User can visit homepage' do
+    visit root_path
+    assert_text 'Welcome to Whisky Tracker'
   end
 end
