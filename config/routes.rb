1c487a753ef7193749a62c2b5cd468f0a1885899
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2021-04-21 17:17:55 +0100

    Sign-up feature test passing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
   root 'home#index'
 end
--- spec/features/sign-up_spec.rb
+++ spec/features/sign-up_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe "FEATURE - sign-up" do
   it "allows a new user to sign-up" do
     visit root_path
-    click_link "Sign-up"
-    fill_in "Username", with: "Johnno"
-    fill_in "First name", with: "John"
-    fill_in "Surname", with: "Smith"
-    fill_in "Email", with: "johnsmith@example.com"
-    fill_in "Password", with: "iloveruby"
-    click_button("Create Account")
-    expect(page).to have_text("Welcome John!")
+    click_link "Login/Sign-up"
+    click_link "Sign up"
+    fill_in "Email", with: "user-1@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button("Sign up")
+    expect(page).to have_text("Welcome user-1@email.com!")
+  end
+end
+
+RSpec.describe "FEATURE - log-in" do
+  xit "allows an existing user to sign-in" do
+    visit root_path
+    click_link "Login/Sign-up"
+    fill_in "Email", with: "user-1@email.com"
+    fill_in "Password", with: "password"
+    click_button("Log in")
+    expect(page).to have_text("Welcome user-1@email.com!")
   end
 end
